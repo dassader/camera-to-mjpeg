487d32e40736337a8ef5b1bf6b02e48d8afd6c5e
dassader <dassader@live.ru>
2021-10-11 01:17:12 +0300

    Add logs user connections

--- src/main/java/com/home/cameratomjpeg/controller/MjpegController.java
+++ src/main/java/com/home/cameratomjpeg/controller/MjpegController.java
@@ -2,6 +2,7 @@ package com.home.cameratomjpeg.controller;
 
 import com.home.cameratomjpeg.service.CameraStreamService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +13,36 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class MjpegController {
 
     private CameraStreamService cameraStreamService;
+    private AtomicInteger atomicInteger;
 
     @GetMapping(value = "/camera/{cameraId}")
     @ResponseBody
-    public void getCameraStream(@PathVariable String cameraId, HttpServletResponse response) throws IOException {
+    public void getCameraStream(@PathVariable String cameraId, HttpServletResponse response) {
         response.setContentType("multipart/x-mixed-replace; boundary=--BoundaryString");
 
-        ServletOutputStream outputStream = response.getOutputStream();
+        log.info("User connected. User count: {}", atomicInteger.incrementAndGet());
 
-        BiConsumer<InputStream, Long> partsConsumer =
-                (inputStream, length) -> writeBoundary(outputStream, inputStream, length);
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
 
-        cameraStreamService.writeCameraSnapshot(cameraId, partsConsumer);
+            BiConsumer<InputStream, Long> partsConsumer =
+                    (inputStream, length) -> writeBoundary(outputStream, inputStream, length);
+
+            cameraStreamService.writeCameraSnapshot(cameraId, partsConsumer);
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+        } finally {
+            log.info("User disconnected. User count: {}", atomicInteger.decrementAndGet());
+        }
     }
 
     private void writeBoundary(ServletOutputStream outputStream, InputStream inputStream, long length) {
